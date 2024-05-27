772a6a1fe948f5687ad28a4f8dca88318eb3e06c
fal2bro <arai261@stu.kanazawa-u.ac.jp>
2024-05-27 13:39:05 +0900

    mydynamixelをrosで動かせるようにした

--- scripts/mydynamixel_control.py
+++ scripts/mydynamixel_control.py
@@ -2,9 +2,13 @@
 
 import rospy
 
-
-from mypkg.msg import SetPosition
+from mypkg.msg import *
 import myDynamixel
+
+
+
+
+
 
 def set_goal_pos_callback(data):
     print("Set Goal Position of ID %s =%s" % (data.id, data.position))
@@ -25,9 +29,9 @@ def set_goal_velocity_callback(data):
     dxl.write(data.id, dxl.Address.TorqueEnable, False)#いらなそう あとで検証
     dxl.Change_OperatingMode(data.id, dxl.operating_mode.velocity_control)
     dxl.write(data.id, dxl.Address.TorqueEnable,True)
-    dxl.write(data.id. dxl.Address.GoalVelocity, data.velocity)
+    dxl.write(data.id, dxl.Address.GoalVelocity, data.velocity)
 
-def set_goal_pos_callback(data):
+def set_goal_ex_pos_callback(data):
     print("Set Extended Goal Position of ID %s =%s" % (data.id, data.position))
     dxl.write(data.id, dxl.Address.TorqueEnable, False)#いらなそう あとで検証
     dxl.Change_OperatingMode(data.id, dxl.operating_mode.extended_Position_control)
@@ -41,7 +45,7 @@ def set_goal_current_base_pos_callback(data):
     dxl.write(data.id, dxl.Address.TorqueEnable, True)
     dxl.write(data.id, dxl.Address.GoalPosition, data.position)
 
-def set_goal_pwm(data):
+def set_goal_pwm_callback(data):
     print("Set pwm Goal Position of ID %s =%s" % (data.id, data.pwm))
     dxl.write(data.id, dxl.Address.TorqueEnable, False)#いらなそう あとで検証
     dxl.Change_OperatingMode(data.id, dxl.operating_mode.pwm_control)
@@ -58,12 +62,13 @@ def mydynamixel_control():
     rospy.Subscriber('position_control',SetPosition,set_goal_pos_callback)
     rospy.Subscriber('current_control',SetCurrent,set_goal_current_callback)
     rospy.Subscriber('velocity_control',SetVelocity,set_goal_velocity_callback)
-    rospy.Subscriber('extended_Position_control',SetPosition,Set_extended_goal_pos_callback)#横着かも
-    rospy.Subscriber('current_base_position_control',SetPwm,Set_goal_pwm_callback)
+    rospy.Subscriber('extended_Position_control',SetPosition,set_goal_ex_pos_callback)#横着かも
+    rospy.Subscriber('current_base_position_control',SetPosition,set_goal_pwm_callback)
 
     rospy.spin()
 
 def main():
+    global dxl
     dxl = myDynamixel.Dxlfunc()
     MotorNum = dxl.init('/dev/ttyUSB0', baudrate=57600)
     if MotorNum >0:
